b5eb0ac1abeef20610f298979225e4e7dd0d36aa
robinson <robinpaulista85@gmail.com>
2023-03-01 17:15:37 -0300

    Inserindo campo de pesquisa

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -25,4 +27,32 @@ def ver_contato(request, contato_id):
     
     return render(request, 'contatos/ver_contato.html',{
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    
+    if termo is None or not termo:
+        raise Http404()
+    
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    
+    print(termo)
+    
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+    
+    print(contatos.query)
+    paginator = Paginator(contatos, 10)
+    
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    
+    return render(request, 'contatos/busca.html',{
+        'contatos': contatos
     })
