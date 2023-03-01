d3bd4578e56f31d6d3572b8d46bff6ee3016d0ed
robinson <robinpaulista85@gmail.com>
2023-03-01 13:55:32 -0300

    Paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contato
+from django.core.paginator import Paginator
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+    
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    
     return render(request, 'contatos/index.html',{
         'contatos': contatos
     })
@@ -15,4 +22,3 @@ def ver_contato(request, contato_id):
     return render(request, 'contatos/ver_contato.html',{
         'contato': contato
     })
-
